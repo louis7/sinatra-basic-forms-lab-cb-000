ea2c0a70e8eeba6628f75f1c73a8b22127c12f63
louis baptiste <louisvis@ymail.com>
2019-03-07 22:27:01 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,10 +4,10 @@ class Puppy
 
 attr_accessor :name,:breed, :age
 
-    def initialize (name:, breed:, age:)
+    def initialize (name:, breed:, months_old:)
       @name = name
       @breed = breed
-      @age = age
+      @age = months_old
     end
 
 
